0e0c31718ff19623d8d444689080cd07c71238c2
colefin8 <cole.fin8@gmail.com>
2019-09-24 14:34:06 -0600

    made a big shrek picture

---
+++ app-5/src/Components/Image.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Image extends Component{
+    render(){
+        return(
+            <div>
+                <img src='https://upload.wikimedia.org/wikipedia/en/4/4d/Shrek_%28character%29.png' alt="big shrek" style={{width: "100000px", height: "100000px"}}/>
+            </div>
+        )
+    }
+}
+
+export default Image
