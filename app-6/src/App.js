5a831c57ef5186e838c6e5c4bcd15f11107afc8c
colefin8 <cole.fin8@gmail.com>
2019-09-24 15:07:31 -0600

    six done

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import ToDo from "./Components/ToDo"
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: '',
+      toDoList: []
+    }
+  }
+
+  handleInput = (e) =>{
+    this.setState({userInput: e})
+  }
+
+  handleSubmit = () => {
+    let tempArr = this.state.toDoList
+    tempArr.push(this.state.userInput)
+    this.setState({toDoList: tempArr})
+  }
+
+  handleRemove = (index) =>{
+    let newArray = this.state.toDoList
+    newArray.splice(index, 1)
+    this.setState({toDoList: newArray})
+  }
+  
   render() {
+    let list = this.state.toDoList.map((element, index)=>{
+      return (<div key={index} className="item">
+        <ToDo task={element}/>
+        <button onClick={()=> this.handleRemove(index)}>Remove</button>
+        </div>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <input onChange={(e)=> this.handleInput(e.target.value)}/>
+          <button onClick={()=>this.handleSubmit()}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {list}
       </div>
     );
   }
