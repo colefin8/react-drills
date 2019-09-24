0d5ea645d2a0800d6771733f5970415f7856d62a
colefin8 <cole.fin8@gmail.com>
2019-09-24 13:53:32 -0600

    done with three

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list:['cole', 'banana', 'coding', 'potato', 'cheese', 'sushi', 'ice cream'],
+      userInput: ""
+    }
+  }
+
+  updateText = (value) =>{
+    this.setState({
+      userInput: value
+    })
+  } 
+  
   render() {
+    const tempArray = this.state.list;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.updateText(e.target.value)}/>
+        {tempArray.map((element) => {
+          if(element.includes(this.state.userInput)){
+          return (<h2>{element}</h2>)
+          }
+        })}
       </div>
     );
   }
