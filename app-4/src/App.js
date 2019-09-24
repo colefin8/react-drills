b24eda8c96ea4b0537c988e63c577fe23d8ba617
colefin8 <cole.fin8@gmail.com>
2019-09-24 14:14:45 -0600

    finished

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.class = {
+      name: '',
+      password: ''
+    }
+  }
+
+  handlePassChange = (value) =>{
+    this.setState({password: value})
+  }
+
+  handleUserChange = (value) =>{
+    this.setState({name: value})
+  }
+
+  submit = () =>{
+    window.alert(`username: ${this.state.name}, password: ${this.state.password}`)
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <input placeholder='username' onChange={(e)=>this.handleUserChange(e.target.value)}/>
+          <br/>
+          <input placeholder='password'onChange={(e)=>this.handlePassChange(e.target.value)}/>
+          <br/>
+          <button onClick={this.submit}>Submit</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
